5114e7085461a88bc815db13fb8595b76ff75e16
Wesley <wesleyyw8@gmail.com>
2016-04-08 09:25:09 -0300

    pagination implemented

--- application.js
+++ application.js
@@ -23,23 +23,44 @@ angular.module('iCasei').factory('Config', [function() {
 		}
 	};
 }]);
-app.controller('PrincipalController', ['$scope','$http','$routeParams', "Config", "$timeout", function($scope,$http, routeParams, Config, $timeout){
+app.controller('PrincipalController', ['$scope','$http','$routeParams', "Config", "$timeout",
+ function($scope,$http, routeParams, Config, $timeout){
 	$scope.animate = false;
 	$scope.isInvalid = false;
 	$scope.movieName = "batman";
-	
-	
+	$scope.currentPage = 1;
+	$scope.loading = false;
 	$scope.searchMovie = function(){
 		if ($scope.movieName.trim() == ""){
 			$scope.isInvalid = true;
 			return;
 		}
+		$scope.currentPage = 1;
 		$scope.animate = true;
+		$scope.loading = true;
 		$timeout(function(){
-			$http.get(Config.endpoints.getMovies+$scope.movieName).then(function(resp){
+			populateMovies();
+		},2000);
+	};
+	function populateMovies(){
+		$scope.movies = [];
+		$timeout(function(){
+			$scope.loading = true;
+			$http.get(Config.endpoints.getMovies+$scope.movieName+Config.endpoints.page+$scope.currentPage).then(function(resp){
 				$scope.movies = resp.data.Search;
 				console.log($scope.movies);
+				$scope.loading = false;
 			});
-		},2000);
-	}
+		},0);
+	};
+	$scope.nextPage = function(){
+		$scope.currentPage++;
+		populateMovies();
+	};
+	$scope.previouslyPage = function(){
+		if ($scope.currentPage > 1){
+			$scope.currentPage--;
+			populateMovies();
+		}
+	};
 }]);
